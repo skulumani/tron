46e95f346afa7326c96c971975d23609f36902b7
Shankar Kulumani <shanks.k@gmail.com>
2022-02-19 10:50:05 -0500

    Add game update logic

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 AI Tron lightcycle game
 
+# TODO 
+ * [ ] unit tests for player and game grid - make sure motion works as expected
+ * [ ] test for visualization
+ * [ ] pygame human player interface
+ 
 Based on hexatron
 
 # References
--- tron.py
+++ tron.py
@@ -9,22 +9,32 @@ class Status(enum.Enum):
 
 class Player:
     # movement possible - square grid - diagonals possible
-    # (dx, dy)
-    STEPS = [(0, -1), # N 
-             (1, -1), # NE
-             (1, 0), # E
+    # (dy, dx) 
+    STEPS = [(-1, 0), # N 
+             (-1, 1), # NE
+             (0, 1), # E
              (1, 1), # SE
-             (0, 1), # S
-             (-1, 1), # SW
-             (-1, 0), # W
+             (1, 0), # S
+             (1, -1), # SW
+             (0, -1), # W
              (-1, -1)] # NW
+    
+    NORTH_FACING = [0, 1, 7]
+    SOUTH_FACING = [3, 4, 5]
+    EAST_FACING = [1, 2, 3]
+    WEST_FACING = [7, 6, 5]
 
-    def __init__(self, x, y, orientation):
+    ORIENTATION = [NORTH_FACING,
+                   EAST_FACING,
+                   SOUTH_FACING,
+                   WEST_FACING]
+
+    def __init__(self, y, x, orientation):
         """Constructor
 
         Args:
-            x (int): X coordinate of player in grid
-            y (int): Y coordinate of player in grid
+            y (int): Y coordinate of player in grid (row)
+            x (int): X coordinate of player in grid (col)
             orientation (int): orientation of player 0 <= orientation <= 7
         """
 
@@ -89,9 +99,9 @@ class Tron:
         Returns:
             observation (dict): from _get_observation - view of game 
         """
-        # grid (0, 0) is in bottom left corner 
+        # grid (0, 0) is in top left corner
         # positive x - move to larger/higher columns (right)
-        # positive y - move to lower/smaller rows (up)
+        # positive y - move to higher/larger rows (down)
         self.grid = np.zeros([self.size, self.size, self.num_players]) # third axis for player state 
         
         # initialize all the players
@@ -102,12 +112,91 @@ class Tron:
         wall_gap = 1
         # wall_distance = np.random.choise(np.arange(2*self.size - 1), self.num_players) # distance along wall boundary
 
-        # initialize only 2 players for now
+        # initialize half of players on top wall and other half on bottom
         # player 1 - bottom wall
         # player 2 - top wall
-        self.players = [
-            Player(np.random.randint(0, self.size), self.size - 1 - wall_gap, np.random.choice([0, 1, 2, 6, 7])),
-            Player(np.random.randint(0, self.size), wall_gap, np.random.choice([2, 3, 4, 5, 6]))]
+        self.players = [ ]
+        x_location = np.random.choice(np.arange(self.size), self.num_players)
+        for x in x_location:
+            y = np.random.choice([self.size - 1 - wall_gap, wall_gap])
+            orientation_options = NORTH_FACING if y > self.halfsize else SOUTH_FACING
+
+            self.players.append(Player(y, x, np.random.choice(orientation_options)))
+
+            # self.players = [
+            #     Player(np.random.randint(0, self.size), self.size - 1 - wall_gap, np.random.choice([0, 1, 2, 6, 7])),
+            #     Player(np.random.randint(0, self.size), wall_gap, np.random.choice([2, 3, 4, 5, 6]))]
+        self._update()
+        observation = self._get_observation()
+        return observation
+    
+    def _update(self):
+        """Define player positions in the grid"""
+        for idx, player in enumerate(self.players):
+            self.grid[player.y, player.x, idx] = 1
+
+    def _get_observation(self):
+        """Return representation of game
+
+        Returns:
+            observation (dict): Dictionary
+                - board (np.array): nD array representing game board m x n x p
+                    m: y coordinate - positive down
+                    n: x coordinate - positive right
+                    p: player locations
+                - positions (tuple): tuple of each player current coordinates
+                - orientations (tuple): tuple of each player orientation
+        """
+        observation = {
+            'board': self.grid.copy(),
+            'positions': tuple([(p.x, p.y) for p in self.players]),
+            'orientations': tuple([p.orientation for p in self.platyers])}
+        return observations
+
+    def move(self, *actions):
+        """Move all the players
+
+        Args:
+            actions (List[int]): List of actions for each player
+                -2: large CCW turn
+                -1: small CCW turn
+                 0: no turn - straight
+                 1: small CW turn
+                 2: big CW turn
+
+        Returns:
+            observation (dict): Same as _get_observation
+            done (bool): True if 1 or all players have crashed
+            status (List[int]): List of the status for each player
+                0: player is valid location
+                1: player crashed into wall
+                2: player crashed into another tail
+        """
+
+        done = False
+        status = [0 for ii in range(self.num_players)]
+
+        # players are valid - move them
+        for player, action in zip(self.players, actions):
+            player.act(action)
+
+        # check if players have crashed
+        for idx, (player, opponent) in enumerate(zip(self.players, reversed(self.players))):
+            status[idx] = self._validate_player(player, opponent)
+
+        if sum(status) > 0:
+            done = True
+
+        if not done:
+            self._update() # update each player
+
+        observation = self._get_observation()
+        return observation, done, status
+
+
+
+
+
 
 
 
