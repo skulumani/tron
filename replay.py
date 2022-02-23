1822915e44acf83a96718ad69564179d5153ba57
Shankar Kulumani <shankar.kulumani@jhuapl.edu>
2022-02-22 21:41:44 -0700

    Build out replay capability

--- replay.py
+++ replay.py
@@ -4,6 +4,7 @@ import pygame
 import argparse
 
 import tron
+import interface
 
 class ReplayInterface():
     
@@ -15,24 +16,49 @@ class ReplayInterface():
 
     def load(self, filename):
         """Load JSON game datafilename"""
+        self.grid, self.states = tron.Tron.load(filename) 
         
-        pass
+        # get total number of time steps
+        # get number of players
+        # assign some random colors for the players
+        self._reset()
 
     def _reset(self):
 
-        rows = 10 
-        cols = 10
         self.running = True
-        self.cellsize = self.WIDTH // rows
-        self.HEIGHT = cols * self.cellsize
+        self.rows = self.grid.shape[0]
+        self.cols = self.grid.shape[1]
+        self.cellsize = self.WIDTH // self.rows
+        self.HEIGHT = self.cols * self.cellsize
 
+        # intialize image array and surface
+        self.image = np.zeros((self.rows, self.cols, 3))
+        self.image.fill(255) # everything white
+        # build obstacles
+        self.image[self.grid[:,:,0] == 1] = interface.COLORS['black']
+
+
+        self.surf = pygame.Surface((self.image.shape[0], self.image.shape[1]))
+        self.scaled_surf = pygame.Surface((self.WIDTH, self.HEIGHT))
+        # text object
+        self.position_text = interface.Text(size=16)
 
         self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption('TRON REPLAY')
         self.clock = pygame.time.Clock()
     
     def _build_board(self):
-        pass
+        # set all white
+        self.image.fill(255)
+        # draw walls v
+        self.image[self.grid[:,:,0] == 1] = interface.COLORS['black']
+
+
+        # draw the players at current time step
+
+
+        # build surface
+        pygame.surfarray.blit_array(self.surf, self.image.swapaxes(0, 1))
 
     def _draw_grid(self, surf):
         pass
@@ -49,8 +75,33 @@ class ReplayInterface():
     def update(self):
         pass
 
+    def _draw_grid(self, surf):
+        """Draw a grid onto the larger surface"""
+        
+        if self.cellsize >= 8:
+            # vertical lines
+            for r in range(self.image.shape[0]):
+                pygame.draw.line(surf, interface.COLORS['gray'], (r*self.cellsize,0), (r*self.cellsize, self.WIDTH))
+
+            # horizontal
+            for c in range(self.image.shape[1]):
+                pygame.draw.line(surf, interface.COLORS['gray'], (0, c*self.cellsize), (self.HEIGHT, c*self.cellsize))
+
+        return surf
+
     def render(self):
-        pass
+        # draw surface
+        self._build_board()
+
+        pygame.transform.scale(self.surf, (self.WIDTH, self.HEIGHT), self.scaled_surf)
+        self.scaled_surf = self._draw_grid(self.scaled_surf)
+
+        # add text
+        # self.scaled_surf.blit(self.position_text.draw(string), (0,0))
+
+        self.window.blit(self.scaled_surf, (0, 0))
+        pygame.display.update()
+
     def _quit(self):
         pygame.quit()
 
@@ -61,7 +112,7 @@ class ReplayInterface():
             self.render()
             self.clock.tick(3)
 
-        self.quit()
+        self._quit()
 
 
 # load a game from command line argument
@@ -72,5 +123,6 @@ class ReplayInterface():
 # contiuous keypress capable
 if __name__ == "__main__":
     replay = ReplayInterface()
+    replay.load('20220222-213143_tron.json')
     replay.run()
 
