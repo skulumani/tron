59586b45296be2afc22bcee097a3fffc59968538
Shankar Kulumani <shankar.kulumani@jhuapl.edu>
2022-02-23 09:14:20 -0700

    Update replay interface

--- replay.py
+++ replay.py
@@ -19,12 +19,27 @@ class ReplayInterface():
         self.grid, self.states = tron.Tron.load(filename) 
         
         # get total number of time steps
+        self.players = [p for p in self.states]
+        self.num_players = len(self.players)
+
         # get number of players
         # assign some random colors for the players
+        if self.num_players <= 2:
+            self.player_colors = [{'head': interface.COLOR_PAIRS[c][0], 'tail': interface.COLOR_PAIRS[c][1]} for c in range(0, self.num_players)]
+        elif self.num_players <= len(interface.COLOR_PAIRS):    
+            self.player_colors = [{'head': interface.COLOR_PAIRS[c][0], 'tail': interface.COLOR_PAIRS[c][1]} for c in np.random.choice(np.arange(1,len(interface.COLOR_PAIRS)), size=self.num_players, replace=False)]
+        else:
+            self.player_colors = [{'head': interface.COLORS[c[0]], 'tail': interface.COLORS[c[1]]} for c in np.random.choice(list(interface.COLORS), size=(self.num_players, 2), replace=False)]
+
+        # first player is always blue
+        self.player_colors[0] = {'head': interface.COLOR_PAIRS[0][0], 'tail': interface.COLOR_PAIRS[0][1]}
+        
+        # get all the states extracted for the players
         self._reset()
 
     def _reset(self):
 
+        self.step = 0 # step for trajectory plotting
         self.running = True
         self.rows = self.grid.shape[0]
         self.cols = self.grid.shape[1]
@@ -36,7 +51,6 @@ class ReplayInterface():
         self.image.fill(255) # everything white
         # build obstacles
         self.image[self.grid[:,:,0] == 1] = interface.COLORS['black']
-
 
         self.surf = pygame.Surface((self.image.shape[0], self.image.shape[1]))
         self.scaled_surf = pygame.Surface((self.WIDTH, self.HEIGHT))
@@ -53,9 +67,15 @@ class ReplayInterface():
         # draw walls v
         self.image[self.grid[:,:,0] == 1] = interface.COLORS['black']
 
+        # draw the players up through current step
+        for idx, color_dict in enumerate(self.player_colors):
+            x = [state['x'] for state in self.players[idx]]
+            y = [state['y'] for state in self.players[idx]]
+            status = [state['status'] for state in self.players[idx]]
 
-        # draw the players at current time step
-
+            # make sure we don't go over max number of states
+            max_step = self.step if self.step <= len(x) else len(x)
+            self.image[y[0:max_step], x[0:max_step], :] = color_dict['tail']
 
         # build surface
         pygame.surfarray.blit_array(self.surf, self.image.swapaxes(0, 1))
@@ -72,6 +92,13 @@ class ReplayInterface():
                 if event.key in (pygame.K_ESCAPE, pygame.K_q):
                     self.running = False
                     break
+                if event.key in (pygame.K_RIGHT,):
+                    self.step += 1
+                if event.key in (pygame.K_LEFT,):
+                    self.step -= 1
+                if event.key in (pygame.K_r,):
+                    self._reset()
+
     def update(self):
         pass
 
@@ -97,7 +124,7 @@ class ReplayInterface():
         self.scaled_surf = self._draw_grid(self.scaled_surf)
 
         # add text
-        # self.scaled_surf.blit(self.position_text.draw(string), (0,0))
+        self.scaled_surf.blit(self.position_text.draw("Step: {}".format(self.step)), (0,0))
 
         self.window.blit(self.scaled_surf, (0, 0))
         pygame.display.update()
