70f3d0ab1b023c6c91cca6a9c2935765c5f0848e
Shankar Kulumani <shanks.k@gmail.com>
2022-03-08 16:39:13 -0500

    Work on some fixed agents

--- random_avoid_agent.py
+++ random_avoid_agent.py
@@ -4,11 +4,27 @@ import tron
 def generate_move(board, positions, orientation):
     """
 
+    Args:
+        board (np.array): game board as nd array
+            [:, :, 0]: obstacles
+            [:, :, 1:]: player locations
+        positions (list): list of current position of self and opponents as tuple (y, x)
+        orientations (list): list of self orientation and opponents
+            from tron.Orientation
+    
+    Returns:
+        move (int): Integer move command from tron.Turn
     """
     pass
 
-def get_valid_moves():
-    # check all moves for future collisions
+def get_valid_moves(board, positions, orientation):
+    # get list of possible moves
+    for a in tron.Turn:
+        (yn, xn, _) = tron.Player.future_move(y, x, orientation, a)
+
+        # check if resulting square is occupied
+
+        # check all moves for future collisions
 
     # randomly pick a move from valid options - or if none pick a random invalid one
 
